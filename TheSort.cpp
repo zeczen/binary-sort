ad0ebfe70f63ed325d373d1a546edace354a64eb
eyal seckbach <54035934+zeczen@users.noreply.github.com>
2021-04-25 21:24:20 +0300

    Update TheSort.cpp

--- TheSort.cpp
+++ TheSort.cpp
@@ -1,10 +1,10 @@
-void insert(int* arr, int size, int item, int index) {
+void _insert(int* arr, int size, int item, int index) {
     int i;
     for(i = size; i>index && i != 0; i--)
         *(arr+i) = *(arr + i-1);
     *(arr+i) = item;
 }
-int pop(int* arr, int size, int index) {
+int _pop(int* arr, int size, int index) {
     int toRet = *(arr +index);
     for (int i = index; i < size; i++){
         int a = *(arr + i);
@@ -13,7 +13,7 @@ int pop(int* arr, int size, int index) {
     }
     return toRet;
 }
-int binaryInsert(int* arr, int size, int search) {
+int _binaryInsert(int* arr, int size, int search) {
     if(size == 0) return 0;
     int right = size - 1, left = 0, mid = size / 2;
 
@@ -31,18 +31,18 @@ int binaryInsert(int* arr, int size, int search) {
             return mid;
     }
  
-    if(right <= left){
-        if(*(arr+mid) < search){
-            return mid + 1;cout << "======" << endl;}
-    }    return mid;
+    if(right <= left)
+        if(*(arr+mid) < search)
+            return mid + 1;
+    return mid;
 
 }
 
 void lion_sort(int* arr, int length){
     int size, element, loc;
     for(size = 1; size < length; size++){
-        element = pop(arr, size, size);
-        loc = binaryInsert(arr, size, element);
-        insert(arr, size, element, loc);
+        element = _pop(arr, size, size);
+        loc = _binaryInsert(arr, size, element);
+        _insert(arr, size, element, loc);
     }  
 }
