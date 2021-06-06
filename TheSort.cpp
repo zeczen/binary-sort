1d7f02ed1d131adc0b15627af8ecbf01f8778de2
eyal seckbach <54035934+zeczen@users.noreply.github.com>
2021-06-06 10:42:21 +0300

    Update TheSort.cpp

--- TheSort.cpp
+++ TheSort.cpp
@@ -1,48 +1,44 @@
-void _insert(int* arr, int size, int item, int index) {
-    int i;
-    for(i = size; i>index && i != 0; i--)
-        *(arr+i) = *(arr + i-1);
-    *(arr+i) = item;
-}
-int _pop(int* arr, int size, int index) {
-    int toRet = *(arr +index);
-    for (int i = index; i < size; i++){
-        int a = *(arr + i);
-        *(arr + i) = *(arr + i + 1);
-        *(arr + i + 1) = a;
-    }
-    return toRet;
-}
-int _binaryInsert(int* arr, int size, int search) {
-    if(size == 0) return 0;
+#include <iostream>
+#include <bits/stdc++.h>
+#include <vector>
+
+using namespace std;
+
+template<typename T>
+int _binaryInsert(vector<T> arr, int size, T search) {
+    if (size == 0) return 0;
     int right = size - 1, left = 0, mid = size / 2;
 
     while (right - left > 0) {
 
-        if (*(arr + mid) > search) { 
+        if (arr.at(mid) > search) {
             right = mid - 1;
             mid = (right + left) / 2;
-        }
-        else if (*(arr + mid) < search) { 
+        } else if (arr.at(mid) < search) {
             left = mid + 1;
             mid = (right + left) / 2;
-        }
-        else if (*(arr + mid) == search) 
+        } else if (arr.at(mid) == search)
             return mid;
     }
- 
-    if(right <= left)
-        if(*(arr+mid) < search)
+
+    if (right <= left)
+        if (arr.at(mid) < search)
             return mid + 1;
     return mid;
 
 }
 
-void lion_sort(int* arr, int length){
-    int size, element, loc;
-    for(size = 1; size < length; size++){
-        element = _pop(arr, size, size);
-        loc = _binaryInsert(arr, size, element);
-        _insert(arr, size, element, loc);
-    }  
+template<typename T>
+vector<T> lion_sort(vector<T> arr) {
+    int i, loc;
+    T element;
+    vector<T> vec;
+    vec.push_back(arr.at(0));
+    for (i = 1; i < arr.size(); i++) {
+        // the sub array- vec[0 : i] is sorted
+        element = arr.at(i);
+        loc = _binaryInsert(vec, i, element);
+        vec.insert(vec.begin() + loc, element);
+    }
+    return vec;
 }
